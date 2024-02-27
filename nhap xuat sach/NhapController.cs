8d7105b6fa65a8013b129c10d3f2501d6af0fb74
DoanThiNgocBichHUFI <97453724+DoanThiNgocBichHUFI@users.noreply.github.com>
2024-02-27 16:02:30 +0700

    Add files via upload
    nhap xuat thong tin sach

---
+++ nhap xuat sach/NhapController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace bai5.Controllers
+{
+    public class NhapController : Controller
+    {
+        //
+        // GET: /Nhap/
+        [HttpGet]
+        public ActionResult Nhap()
+        {
+            return View();
+        }
+
+	}
+}
---
+++ nhap xuat sach/Sach.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace bai5.Models
+{
+    public class Sach
+    {
+        public string ID { get; set; }
+        public string tenSach { get; set; }
+        public string AnhBia { get; set; }
+        public int Gia { get; set; }
+        public Sach(string id, string name, int pri, string avatar)
+        {
+            ID = id;
+            tenSach = name;
+            Gia = pri;
+            AnhBia = avatar;
+        }
+    }
+}
